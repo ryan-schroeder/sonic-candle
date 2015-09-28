1eaf754eb4a440b39dc4cb2c38d3ca81491cb649
Dakkra <contact@dakkra.com>
2015-09-27 21:46:59 -0600

    update windowing defaults

--- src/main/java/net/sf/soniccandle/SonicCandle.java
+++ src/main/java/net/sf/soniccandle/SonicCandle.java
@@ -9,21 +9,21 @@ import net.sf.soniccandle.ui.MainWindow;
 
 public class SonicCandle {
 
-	private static final GraphicsDevice gdScreen = GraphicsEnvironment.getLocalGraphicsEnvironment()
-			.getDefaultScreenDevice();
-	public static final int SCREEN_WIDTH = gdScreen.getDisplayMode().getWidth();
-	public static final int SCREEN_HEIGHT = gdScreen.getDisplayMode().getHeight();
+    private static final GraphicsDevice gdScreen = GraphicsEnvironment.getLocalGraphicsEnvironment()
+            .getDefaultScreenDevice();
+    public static final int SCREEN_WIDTH = gdScreen.getDisplayMode().getWidth();
+    public static final int SCREEN_HEIGHT = gdScreen.getDisplayMode().getHeight();
 
-	public static void main(String[] args) {
-		System.out.println("Initializing...");
+    public static void main(String[] args) {
+        System.out.println("Initializing Sonic Candle...");
 
-		SwingUtilities.invokeLater(() -> initApplication());
+        SwingUtilities.invokeLater(() -> initApplication());
 
-	}
+    }
 
-	private static void initApplication() {
-		MainWindow mw = new MainWindow();
-		mw.show();
-	}
+    private static void initApplication() {
+        MainWindow mw = new MainWindow();
+        mw.show();
+    }
 
 }
--- src/main/java/net/sf/soniccandle/ui/MainWindow.java
+++ src/main/java/net/sf/soniccandle/ui/MainWindow.java
@@ -11,46 +11,54 @@ import net.sf.soniccandle.util.SonicIO;
 
 public class MainWindow {
 
-	private static int mFrameDefaultWidth = (int) Math.round(SonicCandle.SCREEN_WIDTH / 1.5);
-	private static int mFrameDefaultHeight = (int) Math.round(SonicCandle.SCREEN_HEIGHT / 1.5);
-	private static Dimension mFrameDefaultDimension = new Dimension(mFrameDefaultWidth, mFrameDefaultHeight);
-	private static SonicFrame mFrame;
+    private static int mFrameDefaultWidth = (int) Math.round(SonicCandle.SCREEN_WIDTH / 1.5);
+    private static int mFrameDefaultHeight = (int) Math.round(SonicCandle.SCREEN_HEIGHT / 1.5);
+    private static Dimension mFrameDefaultDimension = new Dimension(mFrameDefaultWidth, mFrameDefaultHeight);
+    private static SonicFrame mFrame;
+    private String projectTitle = "Untitled";
 
-	public MainWindow() {
+    public MainWindow() {
 
-		// Use Nimbus
-		try {
-			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-				if ("Nimbus".equals(info.getName())) {
-					UIManager.setLookAndFeel(info.getClassName());
-					break;
-				}
-			}
-		} catch (Exception e) {
-			System.out.println("Nimbus look and feel not found (MainView.java)");
-			e.printStackTrace();
-		}
+        // Use Nimbus
+        try {
+            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Nimbus look and feel not found (MainView.java)");
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	public void show() {
-		mFrame = new SonicFrame();
+    public void show() {
+        mFrame = new SonicFrame();
 
-		mFrame.setVisible(true);
-	}
+        mFrame.setVisible(true);
+    }
 
-	private class SonicFrame extends JFrame {
-		private static final long serialVersionUID = 3519931960287250842L;
+    private class SonicFrame extends JFrame {
+        private static final long serialVersionUID = 3519931960287250842L;
 
-		public SonicFrame() {
-			this.setTitle("Sonic Candle");
-			this.setIconImage(SonicIO.loadResourceAsImage("/sonic-candle-icon.png"));
-			this.setSize(mFrameDefaultDimension);
-			this.setResizable(true);
-			this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        public SonicFrame() {
+            updateFrameTitle(projectTitle);
+            this.setIconImage(SonicIO.loadResourceAsImage("/sonic-candle-icon.png"));
+            this.setSize(mFrameDefaultDimension);
+            this.setMinimumSize(mFrameDefaultDimension);
+            this.setResizable(true);
+            //TODO Make a custom exit to make sure all threads/streams are done
+            this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-		}
+        }
 
-	}
+        public void updateFrameTitle(String newProjectTitle) {
+            projectTitle = newProjectTitle;
+            this.setTitle("Sonic Candle: " + projectTitle);
+        }
+
+    }
 
 }
